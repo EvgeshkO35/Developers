70fee86f998b89114d46cc701fd8fa675c252022
EvgeshkO35 <56804253+EvgeshkO35@users.noreply.github.com>
2021-04-22 15:33:03 -0400

    Initial Commit

---
+++ Learn By Doing/Learn_By_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_By_DoingApp.swift
+//  Learn By Doing
+//
+//  Created by Evgenii Lysenko on 4/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_By_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
